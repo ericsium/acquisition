2d2002e501818ffbddc28267ec8832ba86655be5
Eric White <erwhite99-git@yahoo.com>
2016-04-24 09:30:20 -0500

    Update VER_STR to reflect beta

--- src/version_defines.h
+++ src/version_defines.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define VER_CODE                    29
-#define VER_STR                     "0.5f"
+#define VER_STR                     "0.5f-beta1"
 #define VER_FILEVERSION             0,5,6,0
 #define VER_FILEVERSION_STR         "0.5.6.0"
 
